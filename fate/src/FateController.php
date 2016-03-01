9c9b2ba621170fcb4e90086bc3b8f7f7a1ea342b
Coccaro Daniel <coccaro.daniel@gmail.com>
2016-03-01 15:21:15 -0300

    Comienzo

---
+++ fate/src/FateController.php
@@ -0,0 +1,13 @@
+<?php
+namespace Ydentidad\Fate;
+use App\Http\Controllers\Controller;
+ 
+class FateController extends Controller
+{
+ 
+    public function index()
+    {
+        echo "H1";
+    }
+ 
+}
---
+++ fate/src/routes.php
@@ -0,0 +1,3 @@
+<?php
+Route::get('fate', 
+  'Ydentidad\Fate\FateController@index');
